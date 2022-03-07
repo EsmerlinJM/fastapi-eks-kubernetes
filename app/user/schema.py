6cc1eb47e1e64909c56d7ba7e81b324f181381eb
EsmerlinJM <20164379@itla.edu.do>
2022-03-07 09:07:47 -0400

    [FEAT]: User endpoints improvement.
    - Add `all_users` for get all registered users, 'get_user_by_id' for get user by id and `delete_user_by_id` for delete user by id on `services`.
    - Add new endpoints for get all, get one and delete users on `router`.
    - Add DisplayUser on `schema` for get new info for user.

--- app/user/schema.py
+++ app/user/schema.py
@@ -7,3 +7,12 @@ class User(BaseModel):
     name: constr(min_length=2, max_length=50)
     email: EmailStr
     password: str
+    
+    
+class DisplayUser(BaseModel):
+    id: int
+    name: str
+    email: str
+    
+    class Config:
+        orm_mode = True
